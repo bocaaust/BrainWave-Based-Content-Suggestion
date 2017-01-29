f16b342b96b51aaf447998892304dfda6f2b87bd
Austin Lubetkin <bocaaust@gmail.com>
2017-01-29 02:38:00 -0500

    Redesign and Efficiency

--- Website/MemeMachine/js/index.js
+++ Website/MemeMachine/js/index.js
@@ -59,12 +59,17 @@ function transformDataToParams(data) {
 }
 
 function postImage(imgurl) {
+	if (localStorage.getItem('imgurl') === imgurl) {
+		altParse();
+		//return localStorage.getItem('data');
+	}else{
+		localStorage.setItem('imgurl', imgurl);
 	document.getElementById('originalImage').src = imgurl;
 	
   var accessToken = localStorage.getItem('accessToken');
   var data = {
     'url': imgurl
-  };
+  }; 
   var url = 'https://api.clarifai.com/v1/tag';
   return axios.post(url, data, {
     'headers': {
@@ -76,8 +81,30 @@ function postImage(imgurl) {
     console.log('Sorry, something is wrong: ' + err);
   });
 }
+}
+
+function altParse(){
+	var tags = [];
+	var resp = JSON.parse(localStorage.getItem('data'));
+	 if (resp.status_code === 'OK') {
+    var results = resp.results;
+    tags = results[0].result.tag.classes;
+  } else {
+    console.log('Sorry, something is wrong.');
+  }
+	var url = 'https://api.gfycat.com/v1test/gfycats/search?search_text=';
+	axios.get(url + tags[0].toString() + ',' + tags[2].toString() + ',' + tags[3].toString()  + ',' + tags[Math.floor(3 + (Math.random()*6))].toString()).then(function(r) {
+		//console.log(r.data.gfycats[Math.floor((Math.random() * 10))]);
+		document.getElementById('suggested').src = r.data.gfycats[Math.floor((Math.random() * 10))].gifUrl;
+		document.getElementById('third').hidden = false;
+	}, function(err) {
+    console.log('Sorry, something is wrong: ' + err);
+  });
+  return tags;
+}
 
 function parseResponse(resp) {
+	localStorage.setItem('data',JSON.stringify(resp));
   var tags = [];
   if (resp.status_code === 'OK') {
     var results = resp.results;
@@ -91,9 +118,9 @@ function parseResponse(resp) {
     document.getElementById('tags').innerHTML = tags.toString().replace(/,/g, ', ');
 	//console.log(tags[1].toString());
 	var url = 'https://api.gfycat.com/v1test/gfycats/search?search_text=';
-	axios.get(url + tags[0].toString() + ',' + tags[2].toString()  + ',' + tags[3].toString()).then(function(r) {
-		console.log(r.data.gfycats[0]);
-		document.getElementById('suggested').src = r.data.gfycats[0].gifUrl;
+	axios.get(url + tags[0].toString() + ',' + tags[2].toString() + ',' + tags[3].toString()  + ',' + tags[Math.floor(3 + (Math.random()*6))].toString()).then(function(r) {
+		//console.log(r.data.gfycats[Math.floor((Math.random() * 10))]);
+		document.getElementById('suggested').src = r.data.gfycats[Math.floor((Math.random() * 10))].gifUrl;
 		document.getElementById('third').hidden = false;
 	}, function(err) {
     console.log('Sorry, something is wrong: ' + err);
@@ -102,9 +129,11 @@ function parseResponse(resp) {
 }
 
 function run(imgurl) {
+	if (imgurl !== localStorage.getItem('imgurl')){
 	document.getElementById('output').hidden = false;
 	//document.getElementById('output').className += "animated zoomIn first";
 	document.getElementById('second').hidden = true;
+	}
   if (Math.floor(Date.now() / 1000) - localStorage.getItem('tokenTimeStamp') > 86400 || localStorage.getItem('accessToken') === null) {
     getCredentials(function() {
   postImage(imgurl);
